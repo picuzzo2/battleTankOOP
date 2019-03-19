826eb2744c815988d0abec6b66ac53e9cffe1786
goe <keattisak_w@cmu.ac.th>
2019-03-19 21:35:29 +0700

    create display

---
+++ BattleTank/src/battletank/BattleTank.java
@@ -0,0 +1,12 @@
+package battletank;
+
+public class BattleTank 
+{
+
+   
+    public static void main(String[] args) 
+    {
+        Display display = new Display("Battle Tank", 900, 900);
+    }
+    
+}
